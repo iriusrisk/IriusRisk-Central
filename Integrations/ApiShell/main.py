c3cdea92aa7aee3addcb3a355a8f99cd2e73a8e9
Walter Gildersleeve <wgildersleeve@iriusrisk.com>
2023-09-28 12:37:56 +0200

    minor change to logging

--- Integrations/ApiShell/main.py
+++ Integrations/ApiShell/main.py
@@ -4,7 +4,6 @@ import logging
 # This import does a few things automatically:
 #    * it parses the command line
 #    * it attempts to read one or more configuration files
-#    * it provides a log variable for logging
 #    * all configurations from the command line and configuration files are available on the config variable
 #
 # For further information to the possible parameters, execute this file with the --help parameter
