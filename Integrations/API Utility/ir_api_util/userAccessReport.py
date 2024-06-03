a9c13a15ce003ab4da87d6be06a20f43fa7f0df4
esolis <esolis@iriusrisk.com>
2024-06-03 11:24:32 +0000

    Added config.json to control file output. Added Business Unit Report functionality. Redo

--- Integrations/API Utility/ir_api_util/userAccessReport.py
+++ Integrations/API Utility/ir_api_util/userAccessReport.py
@@ -1,7 +1,22 @@
 import os
 import sys
 import requests
+import json
 from datetime import datetime, timedelta
+
+def read_config(config_path='config.json'):
+    try:
+        with open(config_path, 'r') as config_file:
+            config = json.load(config_file)
+            output_path = os.path.expanduser(config.get('output_path', '~/'))
+            # Create the directory if it doesn't exist
+            os.makedirs(output_path, exist_ok=True)
+            return output_path
+    except (FileNotFoundError, json.JSONDecodeError) as e:
+        print(f"Error reading config file: {e}. Defaulting to home directory.")
+        output_path = os.path.expanduser('~/')
+        os.makedirs(output_path, exist_ok=True)
+        return output_path
 
 class BaseAPI:
     def __init__(self, api_token_path='~/ir/.ir_user_token', instance_domain_path='~/ir/ir_instance_domain'):
@@ -40,6 +55,7 @@ class BaseAPI:
 class UserReport(BaseAPI):
     def __init__(self, api_token_path='~/ir/.ir_user_token', instance_domain_path='~/ir/ir_instance_domain'):
         super().__init__(api_token_path, instance_domain_path)
+        self.output_path = read_config()
 
     def get_active_and_inactive_users(self, days=30):
         active_users = self.get_active_users_within_days(days)
@@ -53,9 +69,7 @@ class UserReport(BaseAPI):
         active_users_set = set(active_users)
         inactive_users = list(set(all_usernames) - active_users_set)
 
-        print(f"Active users in the last {days} days:", active_users)
-        print("Inactive users:", inactive_users)
-        print("")
+        self.save_results(active_users, inactive_users, days)
 
     def get_active_users_within_days(self, days):
         end_date = datetime.now()
@@ -76,6 +90,17 @@ class UserReport(BaseAPI):
         else:
             print("No active users found or error encountered.")
             return []
+
+    def save_results(self, active_users, inactive_users, days):
+        result = {
+            "days": days,
+            "active_users": active_users,
+            "inactive_users": inactive_users
+        }
+        output_file = os.path.join(self.output_path, f"user_report_{days}_days.json")
+        with open(output_file, 'w') as file:
+            json.dump(result, file, indent=4)
+        print(f"Report saved to {output_file}")
 
 def main():
     if len(sys.argv) < 2:
