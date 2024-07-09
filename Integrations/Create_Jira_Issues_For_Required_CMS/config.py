0bcf739600492347b0562f9d927ec8ee677ae5f3
mdavidsonIR <117912155+mdavidsonIR@users.noreply.github.com>
2024-07-09 17:05:17 +0100

    Uploading blank config file

---
+++ Integrations/Create_Jira_Issues_For_Required_CMS/config.py
@@ -0,0 +1,8 @@
+#----IriusRisk----
+domain = 'https://<Insert_IriusRisk_Tenant_Name>.iriusrisk.com'
+sub_url = '/api/v2/projects'
+apitoken = '<Insert_API_Token>' #IriusRisk API token
+head = {'api-token': apitoken}
+
+
+# Please insert values for your domain & api token.
